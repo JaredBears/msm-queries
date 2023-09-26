20054fa11828610aa78f96c10b2915877d25fe58
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-09-26 21:40:18 +0000

    Actors Functionality Added To Do: Directors Functionality

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,11 @@
 class ActorsController < ApplicationController
   def index
+    @actors = Actor.all
+    render(template: 'actors/index')
   end
 
   def actor_details
+    @actor = Actor.where({ :id => params.fetch(:actor_id) }).first
+    render(template: 'actors/show')
   end
 end
