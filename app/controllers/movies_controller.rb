29e0e1feddfdc5d75f035e3380a230b60bc20cd1
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-09-26 21:26:06 +0000

    Movies Functionality Added To Do: Directors and Actors Functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,13 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
+
+    render({template: 'movies/index'})
   end
 
   def movie_details
+    @movie = Movie.where({ :id => params.fetch(:an_id) }).first
+    render({template: 'movies/show'})
   end
 
 end
