0f7b82d050a48862db6318722d15fb898591bb1d
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-09-27 00:17:59 +0000

    Directors Functionality Added

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,22 @@
 class DirectorsController < ApplicationController
   def index
+    @directors = Director.all
+    render(template: 'directors/index')
   end
 
   def youngest
+    youngest = Director.where.not(dob: nil).order(dob: :desc).first
+    redirect_to("/directors/#{youngest[:id]}")
   end
   
   def eldest
+    eldest = Director.where.not(dob: nil).order(dob: :asc).first
+    redirect_to("/directors/#{eldest[:id]}")
   end
 
   def director_details
+    @directors = Director.all
+    @director = Director.find(params[:director_id])
+    render(template: 'directors/show')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
-  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  get("/directors/:director_id", { :controller => "directors", :action => "director_details" })
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
   get("/actors", { :controller => "actors", :action => "index" })
